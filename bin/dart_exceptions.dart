49d3af87771474dfb1411d9a1a516cf4a5bb60b3
matheus-liber <matheuslibergt@gmail.com>
2023-06-02 14:49:28 -0300

    Lidando com erros e exceções

---
+++ bin/dart_exceptions.dart
@@ -0,0 +1,47 @@
+import 'controllers/bank_controller.dart';
+import 'models/account.dart';
+
+// void main() {
+//   // Criando o banco
+//   BankController bankController = BankController();
+
+//   // Adicionando contas
+//   bankController.addAccount(
+//       id: "Ricarth",
+//       account:
+//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+
+//   bankController.addAccount(
+//       id: "Kako",
+//       account:
+//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+
+//   // Fazendo transferência
+//   bool result = bankController.makeTransfer(
+//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+
+//   // Observando resultado
+//   print(result);
+// }
+
+void main(){
+  print("Started main");
+  functionOne();
+  print("Finished main");
+}
+
+void functionOne(){
+  print("Started F01");
+  functionTwo();
+  print("Finished F02");
+}
+
+void functionTwo(){
+  print("Started F02");
+  for(int i = 1; i <=5; i++){
+    print(i);
+    double amount = double.parse("Not a number");
+  }
+  print("Finished F02");
+}
+
