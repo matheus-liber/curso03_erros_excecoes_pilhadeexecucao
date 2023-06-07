a9054cc23607c273337be57dead28032352d8cdb
matheus-liber <matheuslibergt@gmail.com>
2023-06-07 16:12:55 -0300

    Elaborando erros

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -1,47 +1,46 @@
 import 'controllers/bank_controller.dart';
+import 'excepctions/bank_controller_exceptions.dart';
 import 'models/account.dart';
 
-// void main() {
-//   // Criando o banco
-//   BankController bankController = BankController();
-
-//   // Adicionando contas
-//   bankController.addAccount(
-//       id: "Ricarth",
-//       account:
-//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
-
-//   bankController.addAccount(
-//       id: "Kako",
-//       account:
-//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
-
-//   // Fazendo transferência
-//   bool result = bankController.makeTransfer(
-//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-//   // Observando resultado
-//   print(result);
-// }
-
 void main(){
-  print("Started main");
-  functionOne();
-  print("Finished main");
-}
+  // Criando o banco
+  BankController bankController = BankController();
 
-void functionOne(){
-  print("Started F01");
-  functionTwo();
-  print("Finished F02");
-}
+  // Adicionando contas
+  bankController.addAccount(
+      id: "Ricarth",
+      account:
+          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
 
-void functionTwo(){
-  print("Started F02");
-  for(int i = 1; i <=5; i++){
-    print(i);
-    double amount = double.parse("Not a number");
+  bankController.addAccount(
+      id: "Kako",
+      account:
+          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+
+  // Fazendo transferência
+  try{
+    bool result = bankController.makeTransfer(
+        idSender: "Kako", idReceiver: "Ricarth", amount: 770);
+    
+    if (result){
+      print("Transação concluída com sucesso");
+    }
+  } on InvalidSenderIdException catch (e) {
+    print(e);
+    print("O ID ${e.idSender} do remetente não é um ID válido.");
+  } on InvalidReceiverException catch (e) {
+    print(e);
+    print("O ID ${e.idReceiver} do destinatário não é um ID válido.");
+  } on NotAuthenticatedSenderException catch (e) {
+    print(e);
+    print("O remetente '${e.idSender}' não está autenticado.");
+  } on SenderBalanceLowerThanAmountException catch (e) {
+    print(e);
+    print("O remente '${e.idSender}' não pode transferir ${e.amount} tendo como saldo ${e.senderBalance}");
+  } on Exception {
+    print("Algo deu errado");
   }
-  print("Finished F02");
-}
 
+  // Observando resultado
+  
+}
---
+++ bin/excepctions/bank_controller_exceptions.dart
@@ -0,0 +1,44 @@
+class InvalidSenderIdException implements Exception {
+  static const String report = "InvalidSenderIdException";
+  String idSender;
+  InvalidSenderIdException({required this.idSender});
+
+  @override
+  String toString(){
+    return "$report\nID Sender: $idSender";
+  }
+}
+class InvalidReceiverException implements Exception {
+  static const String report = "InvalidReceiverException";
+  String idReceiver;
+  InvalidReceiverException({required this.idReceiver});
+
+  @override
+  String toString(){
+    return "$report\nID Reciever: $idReceiver";
+  }
+}
+class NotAuthenticatedSenderException implements Exception {
+  static const String report = "NotAuthenticatedSenderException";
+  String idSender;
+  NotAuthenticatedSenderException({required this.idSender});
+
+  @override
+  String toString(){
+    return "$report\nID Sender: $idSender";
+  }
+}
+class SenderBalanceLowerThanAmountException implements Exception {
+  static const String report = "SenderBalanceLowerThanAmountException";
+  String idSender;
+  double senderBalance;
+  double amount;
+
+  SenderBalanceLowerThanAmountException(
+    {required this.idSender, required this.senderBalance, required this.amount});
+
+  @override
+  String toString(){
+    return "$report\nID Sender: $idSender\nSender Balance: $senderBalance\nAmount: $amount";
+  }
+}
